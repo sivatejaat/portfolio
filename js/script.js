794af8cb48d81866febc8e93ee2fc87fbac9d639
Sivateja A T <sivatejaat@gmail.com>
2023-02-20 00:10:33 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const header = document.querySelector('header');
+window.addEventListener('scroll', function() {
+    //let offset = window.pageYOffset;
+    //header.style.backgroundPositionY = offset * 0.7 + 'px';
+    header.classList.toggle('sticky', window.scrollY > 100);
+});  
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
